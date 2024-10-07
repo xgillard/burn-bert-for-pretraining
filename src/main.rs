5f090f2f1fa6178d3b1d0f058335a3c3a23b44f3
xaviergillard <xavier.gillard@uclouvain.be>
2024-10-07 21:50:12 +0200

    manneback

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use tinybert_rs::{bert::BertForPreTrainingConfig, data::data_from_csv, train::{t
 use tokenizers::Tokenizer;
 
 fn main() {
+    type Backend = Wgpu;
     let device = WgpuDevice::BestAvailable;
-    //
+    
     let mut tok = Tokenizer::from_pretrained("xaviergillard/parti-pris-v2", None).unwrap();
-
     let mask_id = tok.get_added_vocabulary().get_vocab()["[MASK]"] as usize;
     let pad_id=  tok.get_added_vocabulary().get_vocab()["[PAD]"] as usize;
     let vocab_size = tok.get_vocab_size(false);
@@ -19,9 +19,11 @@ fn main() {
     
     let optimizer = AdamWConfig::new();
     let config = TrainingConfig::new(model, optimizer, "./artifacts/".to_string())
-        .with_mask_token_id(mask_id);
+        .with_mask_token_id(mask_id)
+        .with_batch_size(32)
+        .with_num_epochs(60);
 
 
     let (trainset, validset) = data_from_csv("resources/corpus_partipris_v2.csv", &mut tok, 20);
-    train::<Autodiff<Wgpu>>(config, trainset, validset, device);
+    train::<Autodiff<Backend>>(config, trainset, validset, device);
 }
